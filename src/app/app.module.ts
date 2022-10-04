d1183593ee5012c0a1fe4c727c27644b0e636021
hkrstev <103205287+hkrstev@users.noreply.github.com>
2022-10-04 14:00:13 -0500

    Generated new component....
    Used this command line
    
    npx ng generate component pizza-toppings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PizzaToppingComponent } from './pizza-topping/pizza-topping.component';
+import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PizzaToppingComponent,
+    PizzaToppingsComponent
   ],
   imports: [
     BrowserModule
