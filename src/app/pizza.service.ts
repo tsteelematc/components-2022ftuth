c33f39ac0275ff7b5f97fef751819d6bd86c719b
hkrstev <103205287+hkrstev@users.noreply.github.com>
2022-10-06 13:22:11 -0500

    Added interface and function to load toppings...
    Next lest call it, but we have to learn DI first...

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
 
+interface PizzaToppingDisplay {
+    name: string;
+    price: number;
+    checked: boolean;
+}
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class PizzaService {
 
   constructor() { }
+
+  loadPizzaToppings = (): PizzaToppingDisplay[] => {
+    const pizzaToppingsFromWeb = [
+        { name: 'Pepperoni', price: 1.00},
+        { name: 'Sausage', price: 1.50},
+        { name: 'Onions', price: 0.50},
+    ];
+    return pizzaToppingsFromWeb.map (x => ({
+        ...x,
+        checked: false
+    }));
+  }
+
+  
 }
