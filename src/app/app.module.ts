9864b975b4c266cd088dff84cf18fa554b8f7d5f
hkrstev <103205287+hkrstev@users.noreply.github.com>
2022-10-06 13:04:22 -0500

    Generated pizza service...
    Used this command line:
    
    npx ng generate service pizza --skip-tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PizzaToppingComponent } from './pizza-topping/pizza-topping.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingComponent,
     PizzaToppingsComponent
   ],
   imports: [
