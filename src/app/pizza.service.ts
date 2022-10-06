031bb8a32871c06f0d22a487eba1708ce50021e8
Sam Paugel <shpaugel@madisoncollege.edu>
2022-10-06 13:22:33 -0500

    Added interface and function to load something
    Next lets call it but we have to learn DI first :-O

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@angular/core';
+import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
+
+interface PizzaToppingsDisplay {
+  name: string;
+  price: number;
+  checked: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +13,21 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  loadPizzaToppings = (): PizzaToppingsDisplay[] => { //make a webservice call eventually instaed of the array
+    const pizzaToppingsFromWeb = [
+      {
+        name: 'Pepperoini'
+      , price: 1.5
+      }
+      ,{
+        name: 'Olives'
+      , price: 3
+      }
+    ];
+    return pizzaToppingsFromWeb.map(x => ({
+      ...x
+      , checked: false
+    }));
+  }
 }
