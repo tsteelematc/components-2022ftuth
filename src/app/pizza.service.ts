02889a071e94f6e1e3280cb3c1081aebd7bc4b45
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2022-10-06 13:22:43 -0500

    created service details
    created an interface for the pizza toppings objects and made it all work

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
 
+interface PizzaToppingsDisplay {
+  name: string;
+  price: number;
+  checked: boolean;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PizzaService {
 
   constructor() { }
+
+  loadPizzaToppings = (): PizzaToppingsDisplay[] => {
+    const pizzaToppingsFromWeb = [
+      {
+        name: 'Pepperoni',
+        price: 1.5
+      },
+      {
+        name: 'Olives',
+        price: 3
+      }
+    ];
+    return pizzaToppingsFromWeb.map(x => ({
+      ...x,
+      checked: false
+    }));
+  }
 }
