aca20f1a2df2c46b59d9329ab22f16d2ac809702
micha4211 <micha4211@icloud.com>
2022-10-15 00:04:43 -0500

    Check in often

--- src/app/pizza-crust-type/pizza-crust-type.component.ts
+++ src/app/pizza-crust-type/pizza-crust-type.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { PizzaCrustService, PizzaCrustType } from '../pizza.crust.service';
 
 @Component({
   selector: 'app-pizza-crust-type',
   templateUrl: './pizza-crust-type.component.html',
-  styleUrls: ['./pizza-crust-type.component.css']
+  styleUrls: ['./pizza-crust-type.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PizzaCrustTypeComponent implements OnInit {
 
@@ -25,9 +26,17 @@ export class PizzaCrustTypeComponent implements OnInit {
   // TS 'getter' or read-only property.
   get crust() {
     return this.availablePizzaCrusts
-      .filter(x => x.checked)
-    ;
+      .filter(x => x.checked);
   };
+
+  termsChange(selected: any): void {
+    console.log(
+         selected.target.name,
+         selected.target.value,
+         selected.target.checked
+       );
+         }
+ 
 
   toggleAll = (check: boolean) => this.availablePizzaCrusts =
     this.availablePizzaCrusts.map(x => ({
