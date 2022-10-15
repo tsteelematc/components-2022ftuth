e2a442b40cd855377110a10016dd246a926e7802
micha4211 <micha4211@icloud.com>
2022-10-14 21:42:27 -0500

    Generated new Component
    created pizza crust type component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,14 @@ import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { PizzaCrustTypeComponent } from './pizza-crust-type/pizza-crust-type.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    PizzaCrustTypeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pizza-crust-type/pizza-crust-type.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-crust-type',
+  templateUrl: './pizza-crust-type.component.html',
+  styleUrls: ['./pizza-crust-type.component.css']
+})
+export class PizzaCrustTypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
