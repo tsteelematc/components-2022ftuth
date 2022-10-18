0e04222ea5e9dae7dded24a34e733fa1095f710b
micha4211 <micha4211@icloud.com>
2022-10-18 13:54:40 -0500

    Add new componet
    reduce sort, show service call

--- src/app/surprise-side/surprise-side.component.ts
+++ src/app/surprise-side/surprise-side.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-surprise-side',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SurpriseSideComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private pizzaSVC: PizzaService
+  ) { }
+
+  availableSides: string[] = [];
 
   ngOnInit(): void {
+    this.availableSides = this.pizzaSVC.loadSides();
   }
 
+  sides: string[] = [];
+  groupedSides: [string, number][] = [];
+
+  randomSide = () => {
+    //Generate random number 1-3
+    //update side property based on the 
+    const r = Math.floor(Math.random() * this.availableSides.length);
+
+   /* const newSide = r === 0 
+    ? "Garlic Bread"
+    : r === 1 ? "Salad"
+    : "Soup"
+    ;
+    */
+
+    const newSide = this.availableSides[r];
+
+    this.sides = [
+      ...this.sides,
+      newSide
+    ].sort();
+
+    const m = this.sides.reduce(
+      (acc, x) => acc.set (
+        x
+        , (acc.get(x) ?? 0) + 1 
+      )
+      , new Map<string, number>()
+    );
+        
+    this.groupedSides = [...m];
+    
+
+  };
 }
