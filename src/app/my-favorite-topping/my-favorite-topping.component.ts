d1f775350b05c41354e240da6c4f9d11b03fc668
tsteelematc <tsteele@madisoncollege.edu>
2022-10-18 12:49:33 -0500

    Generated 2 new components...
    npx ng generate component surprise-side
    
    npx ng generate component my-favorite-topping --skip-tests

---
+++ src/app/my-favorite-topping/my-favorite-topping.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-favorite-topping',
+  templateUrl: './my-favorite-topping.component.html',
+  styleUrls: ['./my-favorite-topping.component.css']
+})
+export class MyFavoriteToppingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
