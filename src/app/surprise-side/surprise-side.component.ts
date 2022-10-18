7ec5ceaa927d405b3204b694178f2fbc7c28af70
hkrstev <103205287+hkrstev@users.noreply.github.com>
2022-10-18 13:10:34 -0500

    Working side component...
    show it to my stakeholders and see if they want any changes...

--- src/app/surprise-side/surprise-side.component.ts
+++ src/app/surprise-side/surprise-side.component.ts
@@ -12,4 +12,16 @@ export class SurpriseSideComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  side = "None";
+
+  randomSide = () => {
+
+    // generate a random number between 1 and 3 
+    const r = Math.floor(Math.random() * 3);
+
+    // update the side property based on that number 
+    this.side = r === 0 ? "Garlic Bread" : r === 1 ? "Salad" : "Soup";
+
+  }
+
 }
