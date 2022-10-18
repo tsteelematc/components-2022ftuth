5ab99b0a6f7b5b53f3b2a0fd42089a535fa78392
emjohnson3 <89865940+emjohnson3@users.noreply.github.com>
2022-10-18 12:49:34 -0500

    Generated two new components
    npx ng generate component surprise-side
    
    npx ng generate component my-favorite-topping --skip-tests

---
+++ src/app/surprise-side/surprise-side.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-surprise-side',
+  templateUrl: './surprise-side.component.html',
+  styleUrls: ['./surprise-side.component.css']
+})
+export class SurpriseSideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
